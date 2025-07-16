1fd0bb4a970114f0533c149092f3a44331f70c27
Ali-Developer-06 <alideveloper066@gmail.com>
2025-07-16 17:58:18 +0500

    add meqia query

--- script.js
+++ script.js
@@ -27,13 +27,26 @@ var x = setInterval(function() {
     }
 }, 1000);
 
-
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 20,
     pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
     },
 });
 
